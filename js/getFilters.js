3bf9a98359d09cfd44e7d5dc8e062a490ae7634b
Porter Libby <pelibby16@earlham.edu>
2025-06-01 22:11:45 -0400

    fix untitled to not show

--- js/getFilters.js
+++ js/getFilters.js
@@ -1,24 +1,27 @@
-function getFilters(){
+function getFilters() {
     var group = [];
     var types = [];
-    for (x = 0; x < data.length; x++){
-        let name = data[x]["Suggester"];
-        let type = data[x]["Type"];
-        if (!group.includes(name)){
-            group.push(name);
-            document.getElementById('group').innerHTML += `
-            <input onchange='createWheel(data)' type="checkbox" value="${name}" id="${name}" name="group" />
-            <label for="${name}">${name}</label>
-            <br>
-            `;
-        }
-        if (!types.includes(type)){
-            types.push(type);
-            document.getElementById('type').innerHTML += `
-            <input onchange='createWheel(data)' type="checkbox" value="${type}" id="${type}" name="type" />
-            <label for="${type}">${type}</label>
-            <br>
-            `;
+    for (x = 0; x < data.length; x++) {
+        console.log(data[x]);
+        if (data[x].title != "" && "Title" in data[x]) {
+            let name = data[x]["Suggester"];
+            let type = data[x]["Type"];
+            if (!group.includes(name)) {
+                group.push(name);
+                document.getElementById("group").innerHTML += `
+                <input onchange='createWheel(data)' type="checkbox" value="${name}" id="${name}" name="group" />
+                <label for="${name}">${name}</label>
+                <br>
+                `;
+            }
+            if (!types.includes(type)) {
+                types.push(type);
+                document.getElementById("type").innerHTML += `
+                <input onchange='createWheel(data)' type="checkbox" value="${type}" id="${type}" name="type" />
+                <label for="${type}">${type}</label>
+                <br>
+                `;
+            }
         }
     }
 }
--- js/wheel.js
+++ js/wheel.js
@@ -1,43 +1,48 @@
 var wheel = null;
-function createWheel(){
+function createWheel() {
     console.log(data);
     var filterdata = data;
 
-    if(document.getElementById('unwatched').checked){
-        filterdata = filterdata.filter(item => item["Watched"] == false);
-    }else if(document.getElementById('watched').checked){
-        filterdata = filterdata.filter(item => item["Watched"] == true);
+    if (document.getElementById("unwatched").checked) {
+        filterdata = filterdata.filter((item) => item["Watched"] == false);
+    } else if (document.getElementById("watched").checked) {
+        filterdata = filterdata.filter((item) => item["Watched"] == true);
     }
 
     var groupFilter = [];
-    $("input:checkbox[name=group]:checked").each(function(){
+    $("input:checkbox[name=group]:checked").each(function () {
         groupFilter.push(this.value);
     });
     var typeFilter = [];
-    $("input:checkbox[name=type]:checked").each(function(){
+    $("input:checkbox[name=type]:checked").each(function () {
         typeFilter.push(this.value);
     });
 
-    if (groupFilter.length > 0){
-        filterdata = filterdata.filter(item => groupFilter.includes(item["Suggester"]));
+    if (groupFilter.length > 0) {
+        filterdata = filterdata.filter((item) =>
+            groupFilter.includes(item["Suggester"]),
+        );
     }
-    if (typeFilter.length > 0){
-        filterdata = filterdata.filter(item => typeFilter.includes(item["Type"]));
+    if (typeFilter.length > 0) {
+        filterdata = filterdata.filter((item) =>
+            typeFilter.includes(item["Type"]),
+        );
     }
-    
 
     renderWheel(filterdata);
 }
 
-function renderWheel(input){
+function renderWheel(input) {
     document.getElementById("wheel").innerHTML = "";
     // 1. Configure the wheel's properties:
     wheel_items = [];
-    for (x = 0; x < input.length; x++){
-        wheel_items.push({
-            label: input[x]["Title"],
-            backgroundColor: colors[x % colors.length]
-        });
+    for (x = 0; x < input.length; x++) {
+        if (input[x].title != "" && "Title" in input[x]) {
+            wheel_items.push({
+                label: input[x]["Title"],
+                backgroundColor: colors[x % colors.length],
+            });
+        }
     }
     const props = {
         items: wheel_items,
@@ -45,12 +50,12 @@ function renderWheel(input){
         pointerAngle: 90,
         rotationSpeedMax: 1000,
         rotationResistance: -200,
-    }
+    };
     console.log(props);
-    
+
     // 2. Decide where you want it to go:
     const container = document.getElementById("wheel");
-    
+
     // 3. Create the wheel in the container and initialise it with the props:
     wheel = new spinWheel.Wheel(container, props);
     wheel.onCurrentIndexChange = sound;
